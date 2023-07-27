a09f9523a3d068bb33766326d5902dda575a4be9
runhey <2234044577@qq.com>
2023-07-28 00:37:48 +0800

    feat(rich man): add charisma store

--- tasks/RichMan/mall/charisma.py
+++ tasks/RichMan/mall/charisma.py
@@ -6,16 +6,33 @@ import time
 from module.logger import logger
 
 from tasks.GameUi.page import page_main, page_guild
-from tasks.GameUi.game_ui import GameUi
-from tasks.Component.Buy.buy import Buy
-from tasks.RichMan.assets import RichManAssets
+from tasks.RichMan.mall.friendship_points import FriendshipPoints
 from tasks.RichMan.config import Charisma as CharismaConfig
 
 
-class Charisma(GameUi, Buy, RichManAssets):
+class Charisma(FriendshipPoints):
 
     def execute_charisma(self, con: CharismaConfig = None):
-        pass
+        if not con:
+            logger.info('Charisma is not enable')
+            con = self.config.rich_man.charisma
+        if not con.enable:
+            logger.info('Charisma is not enable')
+            return
+        self._enter_charisma()
+
+        # 开始
+        # 黑色碎片
+        if con.black_daruma_scrap:
+            self.buy_mall_more(buy_button=self.I_CH_BLACK, remain_number=True,
+                               money_ocr=self.O_MALL_RESOURCE_5,
+                               buy_number=2, buy_max=2, buy_money=320)
+        # 蓝票
+        if con.mystery_amulet:
+            self.buy_mall_one(buy_button=self.I_CH_BLUE, buy_check=self.I_CH_CHECK_BLUE,
+                              money_ocr=self.O_MALL_RESOURCE_5, buy_money=400)
+
+
 
 
 if __name__ == '__main__':
