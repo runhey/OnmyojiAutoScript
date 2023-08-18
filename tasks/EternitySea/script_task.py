747b1f6d64fb3a223bf59516b7dc109dcc0d7177
runhey <2234044577@qq.com>
2023-08-18 18:14:07 +0800

    fix(task): Make mistakes when starting unfinished tasks

--- tasks/EternitySea/script_task.py
+++ tasks/EternitySea/script_task.py
@@ -8,37 +8,26 @@ from module.logger import logger
 from module.base.timer import Timer
 
 from tasks.GameUi.game_ui import GameUi
-from tasks.GameUi.page import page_main, page_team, page_shikigami_records
+from tasks.GameUi.page import page_main, page_soul_zones, page_shikigami_records
 from tasks.Component.GeneralBattle.general_battle import GeneralBattle
 from tasks.Component.GeneralBattle.config_general_battle import GeneralBattleConfig
 from tasks.Component.GeneralRoom.general_room import GeneralRoom
 from tasks.Component.GeneralInvite.general_invite import GeneralInvite
 from tasks.Component.SwitchSoul.switch_soul import SwitchSoul
-from tasks.ExperienceYoukai.assets import ExperienceYoukaiAssets
+from tasks.EternitySea.assets import EternitySeaAssets
 
-class ScriptTask(GameUi, GeneralBattle, GeneralRoom, GeneralInvite, SwitchSoul, ExperienceYoukaiAssets):
+class ScriptTask(GameUi, GeneralBattle, GeneralRoom, GeneralInvite, SwitchSoul, EternitySeaAssets):
 
     def run(self):
-        con = self.config.experience_youkai
+        con = self.config.eternity_sea.eternity_sea_config
         self.ui_get_current_page()
-        self.ui_goto(page_team)
-        self.check_zones('经验妖怪')
+        self.ui_goto(page_soul_zones)
 
-        self.exp_exit()
+        self.set_next_run(task='EternitySea', success=True, finish=False)
+        raise TaskEnd('EternitySea')
 
-    def exp_exit(self, con=None):
-        self.ui_get_current_page()
-        self.ui_goto(page_main)
-        # if con.buff_gold_50_click or con.buff_gold_100_click:
-        #     self.open_buff()
-        #     if con.buff_gold_50_click:
-        #         self.gold_50(False)
-        #     if con.buff_gold_100_click:
-        #         self.gold_100(False)
-        #     self.close_buff()
 
-        self.set_next_run(task='ExperienceYoukai', success=True, finish=False)
-        raise TaskEnd('ExperienceYoukai')
+
 
 if __name__ == '__main__':
     from module.config.config import Config
