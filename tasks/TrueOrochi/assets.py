b5e1b972ef2ae02f1bf3e2be31f06e0ce42bf5a1
runhey <2234044577@qq.com>
2023-08-19 00:55:07 +0800

    feat(task): Add True-Orochi

--- tasks/TrueOrochi/assets.py
+++ tasks/TrueOrochi/assets.py
@@ -54,5 +54,9 @@ class TrueOrochiAssets:
 	I_ST_GREEN_5 = RuleImage(roi_front=(841,335,26,44), roi_back=(814,302,100,100), threshold=0.8, method="Template matching", file="./tasks/TrueOrochi/st/st_st_green_5.png")
 	# description 
 	I_ST_GREEN_6 = RuleImage(roi_front=(930,247,24,47), roi_back=(894,224,100,100), threshold=0.8, method="Template matching", file="./tasks/TrueOrochi/st/st_st_green_6.png")
+	# 60体力创建 
+	I_ST_CREATE_ROOM = RuleImage(roi_front=(528,482,223,62), roi_back=(528,482,223,62), threshold=0.8, method="Template matching", file="./tasks/TrueOrochi/st/st_st_create_room.png")
+	# 头像框 
+	I_ST_FRAME = RuleImage(roi_front=(574,441,130,63), roi_back=(574,441,130,63), threshold=0.8, method="Template matching", file="./tasks/TrueOrochi/st/st_st_frame.png")
 
 
