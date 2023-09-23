31f7746834f9d21d70228611244c1deb9e73e0da
runhey <2234044577@qq.com>
2023-09-23 12:43:24 +0800

    fix(Component): Update  "Buy"

--- tasks/Component/Buy/assets.py
+++ tasks/Component/Buy/assets.py
@@ -12,22 +12,22 @@ class BuyAssets:
 
 	# Click Rule Assets
 	# description 
-	C_BUY_ONE = RuleClick(roi_front=(553,435,174,61), roi_back=(553,435,174,61), name="buy_one")
+	C_BUY_ONE = RuleClick(roi_front=(551,506,174,36), roi_back=(551,506,174,36), name="buy_one")
 	# description 
-	C_BUY_MORE = RuleClick(roi_front=(551,510,174,60), roi_back=(551,510,174,60), name="buy_more")
+	C_BUY_MORE = RuleClick(roi_front=(551,540,174,40), roi_back=(551,540,174,40), name="buy_more")
 	# 取消购买 
 	C_BUY_CANCEL = RuleClick(roi_front=(121,2,100,22), roi_back=(121,2,100,22), name="buy_cancel")
 
 
 	# Image Rule Assets
 	# 拉满 
-	I_BUY_PLUS = RuleImage(roi_front=(759,422,50,58), roi_back=(746,397,98,96), threshold=0.8, method="Template matching", file="./tasks/Component/Buy/buy/buy_buy_plus.png")
+	I_BUY_PLUS = RuleImage(roi_front=(759,422,50,58), roi_back=(746,397,98,156), threshold=0.8, method="Template matching", file="./tasks/Component/Buy/buy/buy_buy_plus.png")
 	# 加一个 
-	I_BUY_ADD = RuleImage(roi_front=(671,428,51,54), roi_back=(645,412,111,84), threshold=0.8, method="Template matching", file="./tasks/Component/Buy/buy/buy_buy_add.png")
+	I_BUY_ADD = RuleImage(roi_front=(671,428,51,54), roi_back=(645,412,111,141), threshold=0.8, method="Template matching", file="./tasks/Component/Buy/buy/buy_buy_add.png")
 	# 减 
-	I_BUY_SUB = RuleImage(roi_front=(467,429,50,48), roi_back=(452,412,85,94), threshold=0.8, method="Template matching", file="./tasks/Component/Buy/buy/buy_buy_sub.png")
+	I_BUY_SUB = RuleImage(roi_front=(467,429,50,48), roi_back=(452,412,85,147), threshold=0.8, method="Template matching", file="./tasks/Component/Buy/buy/buy_buy_sub.png")
 	# 出现用魂玉购买 
-	I_BUY_RMB = RuleImage(roi_front=(610,532,35,32), roi_back=(548,513,178,72), threshold=0.8, method="Template matching", file="./tasks/Component/Buy/buy/buy_buy_rmb.png")
+	I_BUY_RMB = RuleImage(roi_front=(610,532,35,32), roi_back=(548,483,178,119), threshold=0.8, method="Template matching", file="./tasks/Component/Buy/buy/buy_buy_rmb.png")
 
 
 	# Ocr Rule Assets
