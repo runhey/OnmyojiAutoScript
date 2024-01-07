f92dc9a5473b9261927a6471161db9fd19177fd5
lizhenxi <xbj1108_25@126.com>
2024-01-07 21:35:48 +0800

    feat(tasks): implement a simple script task for EternitySea

--- tasks/EternitySea/script_task.py
+++ tasks/EternitySea/script_task.py
@@ -1,32 +1,89 @@
 # This Python file uses the following encoding: utf-8
 # @author runhey
 # github https://github.com/runhey
-from cached_property import cached_property
+from datetime import time, datetime, timedelta
 
 from module.exception import TaskEnd
 from module.logger import logger
-from module.base.timer import Timer
 
 from tasks.GameUi.game_ui import GameUi
-from tasks.GameUi.page import page_main, page_soul_zones, page_shikigami_records
+from tasks.GameUi.page import page_soul_zones
 from tasks.Component.GeneralBattle.general_battle import GeneralBattle
-from tasks.Component.GeneralBattle.config_general_battle import GeneralBattleConfig
 from tasks.Component.GeneralRoom.general_room import GeneralRoom
 from tasks.Component.GeneralInvite.general_invite import GeneralInvite
 from tasks.Component.SwitchSoul.switch_soul import SwitchSoul
 from tasks.EternitySea.assets import EternitySeaAssets
+from tasks.Orochi.config import UserStatus
 
 class ScriptTask(GameUi, GeneralBattle, GeneralRoom, GeneralInvite, SwitchSoul, EternitySeaAssets):
 
     def run(self):
-        con = self.config.eternity_sea.eternity_sea_config
+        config = self.config.model.eternity_sea
+        
         self.ui_get_current_page()
         self.ui_goto(page_soul_zones)
 
-        self.set_next_run(task='EternitySea', success=True, finish=False)
+        match config.eternity_sea_config.user_status:
+            case UserStatus.ALONE:
+                success = self.run_alone()
+            case _:
+                logger.error('Unknown user status')
+
+        if success:
+            self.set_next_run('EternitySea', finish=True, success=True)
+        else:
+            self.set_next_run('EternitySea', finish=False, success=False)
+
         raise TaskEnd('EternitySea')
 
+    def run_alone(self):
+        config = self.config.model.eternity_sea
 
+        logger.info('Start run alone')
+        self.ui_get_current_page()
+        self.ui_goto(page_soul_zones)
+
+        self.enter_eternity_sea()
+
+        def is_in_eternity_sea(screenshot=False) -> bool:
+            if screenshot:
+                self.screenshot()
+            return self.appear(self.I_ETERNITY_SEA_FIRE)
+        
+        limit_time = config.eternity_sea_config.limit_time
+        limit_datetime: timedelta = timedelta(hours=limit_time.hour, minutes=limit_time.minute, seconds=limit_time.second)
+
+        while 1:
+            self.screenshot()
+
+            if not is_in_eternity_sea():
+                continue
+
+            if self.current_count >= config.eternity_sea_config.limit_count:
+                logger.info('EternitySea count limit out')
+                break
+            if datetime.now() - self.start_time >= limit_datetime:
+                logger.info('EternitySea time limit out')
+                break
+
+            # 点击挑战
+            while 1:
+                self.screenshot()
+                if self.appear_then_click(self.I_ETERNITY_SEA_FIRE, interval=1):
+                    pass
+
+                if not self.appear(self.I_ETERNITY_SEA_FIRE):
+                    self.run_general_battle(config=config.general_battle_config)
+                    break
+
+    def enter_eternity_sea(self):
+        logger.info('Enter eternity_sea')
+        while True:
+            self.screenshot()
+            if self.appear(self.I_FORM_TEAM):
+                return True
+            if self.appear_then_click(self.I_ETERNITY_SEA, interval=1):
+                continue
 
 
 if __name__ == '__main__':
