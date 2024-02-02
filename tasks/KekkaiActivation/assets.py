0180ec26da11723220ba56eb1dce9586d572d42e
huangrunheng <2234044577@qq.com>
2024-02-02 15:53:12 +0800

    fix(KekkaiActivation): Update images #187

--- tasks/KekkaiActivation/assets.py
+++ tasks/KekkaiActivation/assets.py
@@ -78,13 +78,13 @@ class KekkaiActivationAssets:
 
 	# Image Rule Assets
 	# description 
-	I_CARDS_KAIKO_6 = RuleImage(roi_front=(191,216,89,73), roi_back=(169,148,130,501), threshold=0.8, method="Template matching", file="./tasks/KekkaiActivation/cards/cards_cards_kaiko_6.png")
+	I_CARDS_KAIKO_6 = RuleImage(roi_front=(193,192,89,73), roi_back=(169,148,130,501), threshold=0.8, method="Template matching", file="./tasks/KekkaiActivation/cards/cards_cards_kaiko_6.png")
 	# description 
-	I_CARDS_KAIKO_5 = RuleImage(roi_front=(192,255,88,22), roi_back=(172,162,126,488), threshold=0.9, method="Template matching", file="./tasks/KekkaiActivation/cards/cards_cards_kaiko_5.png")
+	I_CARDS_KAIKO_5 = RuleImage(roi_front=(191,345,88,58), roi_back=(172,162,126,488), threshold=0.9, method="Template matching", file="./tasks/KekkaiActivation/cards/cards_cards_kaiko_5.png")
 	# description 
-	I_CARDS_KAIKO_4 = RuleImage(roi_front=(191,248,90,22), roi_back=(179,165,116,482), threshold=0.9, method="Template matching", file="./tasks/KekkaiActivation/cards/cards_cards_kaiko_4.png")
+	I_CARDS_KAIKO_4 = RuleImage(roi_front=(192,354,90,22), roi_back=(179,165,116,482), threshold=0.9, method="Template matching", file="./tasks/KekkaiActivation/cards/cards_cards_kaiko_4.png")
 	# description 
-	I_CARDS_KAIKO_3 = RuleImage(roi_front=(191,370,89,66), roi_back=(183,169,109,474), threshold=0.8, method="Template matching", file="./tasks/KekkaiActivation/cards/cards_cards_kaiko_3.png")
+	I_CARDS_KAIKO_3 = RuleImage(roi_front=(192,187,89,66), roi_back=(183,169,109,474), threshold=0.8, method="Template matching", file="./tasks/KekkaiActivation/cards/cards_cards_kaiko_3.png")
 	# description 
 	I_CARDS_FISH_6 = RuleImage(roi_front=(193,183,87,91), roi_back=(178,169,110,474), threshold=0.8, method="Template matching", file="./tasks/KekkaiActivation/cards/cards_cards_fish_6.png")
 	# description 
