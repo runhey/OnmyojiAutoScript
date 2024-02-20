f0ed15093a47a6a0c9fea8aff55b0ddfe3874435
huangrunheng <2234044577@qq.com>
2024-02-20 14:47:39 +0800

    feat(costume main): Add '结缘神社' #195

--- tasks/Component/Costume/assets.py
+++ tasks/Component/Costume/assets.py
@@ -114,3 +114,16 @@ class CostumeAssets:
 	I_PET_HOUSE_8 = RuleImage(roi_front=(1110,499,50,40), roi_back=(1033,469,241,83), threshold=0.8, method="Template matching", file="./tasks/Component/Costume/main8/main8_pet_house_8.png")
 
 
+	# Image Rule Assets
+	# description 
+	I_CHECK_MAIN_9 = RuleImage(roi_front=(764,117,134,42), roi_back=(149,109,1099,100), threshold=0.8, method="Template matching", file="./tasks/Component/Costume/main9/main9_check_main_9.png")
+	# description 
+	I_MAIN_GOTO_EXPLORATION_9 = RuleImage(roi_front=(553,107,31,30), roi_back=(234,98,904,68), threshold=0.7, method="Template matching", file="./tasks/Component/Costume/main9/main9_main_goto_exploration_9.png")
+	#  
+	I_MAIN_GOTO_SUMMON_9 = RuleImage(roi_front=(970,210,35,29), roi_back=(609,194,623,68), threshold=0.7, method="Template matching", file="./tasks/Component/Costume/main9/main9_main_goto_summon_9.png")
+	# description 
+	I_MAIN_GOTO_TOWN_9 = RuleImage(roi_front=(684,342,41,42), roi_back=(276,321,999,78), threshold=0.8, method="Template matching", file="./tasks/Component/Costume/main9/main9_main_goto_town_9.png")
+	# description 
+	I_PET_HOUSE_9 = RuleImage(roi_front=(915,351,40,35), roi_back=(755,319,405,93), threshold=0.8, method="Template matching", file="./tasks/Component/Costume/main9/main9_pet_house_9.png")
+
+
