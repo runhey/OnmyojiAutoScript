ef104ed2aaf3bcc2fb76029ec0b4bfd24fe6555e
jxjyzzc <jxjyzzc@163.com>
2024-05-07 18:45:27 +0800

    fix[GeneralInvite]:image.json update

--- tasks/RyouToppa/script_task.py
+++ tasks/RyouToppa/script_task.py
@@ -301,7 +301,7 @@ if __name__ == "__main__":
     from module.config.config import Config
     from module.device.device import Device
 
-    config = Config('oas1')
+    config = Config('oas2')
     device = Device(config)
     t = ScriptTask(config, device)
     t.run()
