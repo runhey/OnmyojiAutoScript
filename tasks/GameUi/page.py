d251c7f882ae840c04bbf2d7ef24972d7c68e297
YLXJ <ylxj.xjyl@gmail.com>
2024-09-08 02:28:36 +0800

    增加从鬼兵演武界面前往式神录的路径

--- tasks/GameUi/page.py
+++ tasks/GameUi/page.py
@@ -134,6 +134,7 @@ page_shikigami_records = Page(G.I_CHECK_RECORDS)
 page_shikigami_records.additional = [G.I_AD_DISAPPEAR, G.I_RECORDS_CLOSE]
 page_shikigami_records.link(button=G.I_BACK_Y, destination=page_main)
 page_main.link(button=G.I_MAIN_GOTO_SHIKIGAMI_RECORDS, destination=page_shikigami_records)
+page_guibingyanwu.link(button=G.I_GBYW_GOTO_SHIKIGAMI_RECORDS, destination=page_shikigami_records)
 # 阴阳术 onmyodo
 page_onmyodo = Page(G.I_CHECK_ONMYODO)
 page_onmyodo.link(button=G.I_BACK_Y, destination=page_main)
