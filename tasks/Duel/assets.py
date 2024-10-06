5d2de9fc7c8cd7f07a7000078d07637c9a7a3be5
yEs1do <yangjiaxiyou@gmail.com>
2024-10-07 02:02:35 +0800

    扩大分数识别区域避免OCR错误

--- tasks/Duel/assets.py
+++ tasks/Duel/assets.py
@@ -60,7 +60,7 @@ class DuelAssets:
 	# 荣誉 
 	O_D_HONOR = RuleOcr(roi=(699,562,131,31), area=(699,562,131,31), mode="DigitCounter", method="Default", keyword="", name="d_honor")
 	# 分数 
-	O_D_SCORE = RuleOcr(roi=(701,516,56,26), area=(701,516,56,26), mode="Digit", method="Default", keyword="", name="d_score")
+	O_D_SCORE = RuleOcr(roi=(701,509,61,33), area=(701,509,61,33), mode="Digit", method="Default", keyword="", name="d_score")
 	# 手动 
 	O_D_HAND = RuleOcr(roi=(31,639,63,45), area=(31,639,63,45), mode="Single", method="Default", keyword="手动", name="d_hand")
 	# 自动 
