4deb58d0677e47fe6ab6b52203b32e40df219075
ryland <2234044577@qq.com>
2024-10-22 11:02:20 +0800

    feat(WantedQuests): Customize the battle order

--- tasks/WantedQuests/script_task.py
+++ tasks/WantedQuests/script_task.py
@@ -208,12 +208,16 @@ class ScriptTask(WQExplore, SecretScriptTask, WantedQuestsAssets):
                 return None
             result[1] = wq_destination
             result[2] = wq_number
-            if type_wq == '挑战':
-                result[0] = 0 if num_challenge >= 10 else -1
-            elif type_wq == '秘闻':
-                result[0] = 1
-            elif type_wq == '探索':
-                result[0] = 2
+            order_list = self.config.model.wanted_quests.wanted_quests_config.battle_priority
+            order_list = order_list.replace(' ', '').replace('\n', '')
+            order_list: list = re.split(r'>', order_list)
+            result[0] = order_list.index(type_wq) if type_wq in order_list else -1
+            # if type_wq == '挑战':
+            #     result[0] = 0 if num_challenge >= 10 else -1
+            # elif type_wq == '秘闻':
+            #     result[0] = 1
+            # elif type_wq == '探索':
+            #     result[0] = 2
             logger.info(f'[Wanted Quests] type: {type_wq} destination: {wq_destination} number: {wq_number} ')
             return tuple(result) if result[0] != -1 else None
 
