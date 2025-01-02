97e7eabef4fa89479975ae8b953a58e4fb73f48d
ryland <2234044577@qq.com>
2025-01-02 21:40:56 +0800

    feat(ActivityShikigami): Update

--- tasks/ActivityShikigami/config.py
+++ tasks/ActivityShikigami/config.py
@@ -18,15 +18,14 @@ class ShikigamiType(str, Enum):
     FOOD_TYPE_3 = 'shikigami_type_3'
     FOOD_TYPE_4 = 'shikigami_type_4'
 
-class ShikigamiConfig(BaseModel):
+# class ShikigamiConfig(BaseModel):
     # limit_time: TimeDelta = Field(default=TimeDelta(hours=0, minutes=30, seconds=0), description='limit_time_help')
     # limit_count: int = Field(default=10, description='limit_count_help')
-    food_type: ShikigamiType = Field(default=ShikigamiType.FOOD_TYPE_1, description='food_type_help')
-
+    # food_type: ShikigamiType = Field(default=ShikigamiType.FOOD_TYPE_1, description='food_type_help')
 
 class ActivityShikigami(ConfigBase):
     scheduler: Scheduler = Field(default_factory=Scheduler)
-    shikigami: ShikigamiConfig = Field(default_factory=ShikigamiConfig)
+    # shikigami: ShikigamiConfig = Field(default_factory=ShikigamiConfig)
     general_climb: GeneralClimb = Field(default_factory=GeneralClimb)
     general_battle: GeneralBattleConfig = Field(default_factory=GeneralBattleConfig)
     switch_soul_config: SwitchSoulConfig = Field(default_factory=SwitchSoulConfig)
