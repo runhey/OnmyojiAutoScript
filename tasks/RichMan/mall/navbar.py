0f208f86b880a523551046f4520ee8a7cffdb63e
liujx54704 <liujx54704@hundsun.com>
2025-01-08 15:02:11 +0800

    大富翁  修复寄售屋

--- tasks/RichMan/mall/navbar.py
+++ tasks/RichMan/mall/navbar.py
@@ -1,6 +1,7 @@
 # This Python file uses the following encoding: utf-8
 # @author runhey
 # github https://github.com/runhey
+import re
 import time
 
 from module.logger import logger
@@ -106,6 +107,8 @@ class MallNavbar(GameUi, RichManAssets):
         }
         self.screenshot()
         result = match[index].ocr(self.device.image)
+        match = re.search(r'\d+', result)
+        result = int(match.group())
         if not isinstance(result, int):
             logger.warning(f'Get mall resource {index} error, result: {result}')
         if result == 0:
