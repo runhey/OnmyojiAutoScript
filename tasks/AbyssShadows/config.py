343d361967ab665ae6039a3542308dc26dc87eca
ChengRanORZ <luorq0533@qq.com>
2025-01-10 23:53:48 +0800

    refactor(AbyssShadows):init

--- tasks/AbyssShadows/config.py
+++ tasks/AbyssShadows/config.py
@@ -3,18 +3,185 @@
 # @author   jackyhwei
 # @note     draft version without full test
 # github    https://github.com/roarhill/oas
+from datetime import date, datetime
+from enum import Enum
+from pathlib import Path
 
 from pydantic import BaseModel, Field
+
+from tasks.AbyssShadows.assets import AbyssShadowsAssets
 from tasks.Component.GeneralBattle.config_general_battle import GeneralBattleConfig
 from tasks.Component.SwitchSoul.switch_soul_config import SwitchSoulConfig
-from tasks.Component.config_base import ConfigBase, Time
+from tasks.Component.config_base import ConfigBase, Time, DateTime
 from tasks.Component.config_scheduler import Scheduler
+from cached_property import cached_property
+
+
+class AreaType:
+    """ 暗域类型 """
+    DRAGON = AbyssShadowsAssets.I_ABYSS_DRAGON  # 神龙暗域
+    PEACOCK = AbyssShadowsAssets.I_ABYSS_PEACOCK  # 孔雀暗域
+    FOX = AbyssShadowsAssets.I_ABYSS_FOX  # 白藏主暗域
+    LEOPARD = AbyssShadowsAssets.I_ABYSS_LEOPARD  # 黑豹暗域
+
+    @cached_property
+    def name(self) -> str:
+        """
+
+        :return:
+        """
+        return Path(self.file).stem.upper()
+
+    def __str__(self):
+        return self.name
+
+    __repr__ = __str__
+
+
+class CilckArea:
+    """ 点击区域 """
+    GENERAL_1 = AbyssShadowsAssets.C_GENERAL_1_CLICK_AREA
+    GENERAL_2 = AbyssShadowsAssets.C_GENERAL_2_CLICK_AREA
+    ELITE_1 = AbyssShadowsAssets.C_ELITE_1_CLICK_AREA
+    ELITE_2 = AbyssShadowsAssets.C_ELITE_2_CLICK_AREA
+    ELITE_3 = AbyssShadowsAssets.C_ELITE_3_CLICK_AREA
+    BOSS = AbyssShadowsAssets.C_BOSS_CLICK_AREA
+
+    @cached_property
+    def name(self) -> str:
+        """
+
+        :return:
+        """
+        return Path(self.file).stem.upper()
+
+    def __str__(self):
+        return self.name
+
+    __repr__ = __str__
+
+
+class EnemyType(Enum):
+    """ 敌人类型 """
+    BOSS = 1  # 首领
+    GENERAL = 2  # 副将
+    ELITE = 3  # 精英
+
 
 class AbyssShadowsTime(ConfigBase):
     # 自定义运行时间
     custom_run_time_friday: Time = Field(default=Time(hour=19, minute=0, second=0))
     custom_run_time_saturday: Time = Field(default=Time(hour=19, minute=0, second=0))
     custom_run_time_sunday: Time = Field(default=Time(hour=19, minute=0, second=0))
+    # 尝试主动开启狭间-区别于游戏中的自动开启狭间功能
+    try_start_abyss_shadows: bool = Field(default=False, description='try_start_abyss_shadows_help')
+
+
+class ProcessManage(ConfigBase):
+    # 攻击顺序 A,B,C,D 分别表示四个区域，123456表示区域内6个怪物，从上到下，从左到右的顺序
+    #           1
+    #       2       3
+    #   4       5       6
+    # 之间用-分隔，不同怪物用;分隔
+    # 小蛇使用E,-后面表示打几只,例如E-2表示打两只小蛇
+    # 例如 A-1;B-2;C-3...
+    attack_order: str = Field(default='', description='attack_order_help')
+    # 标记主怪,与攻击顺序类似,可以根据类单独设置
+    # 例如 A;2;3;C-4;D-6
+    mark_main: str = Field(default='', description='mark_boss_help')
+    # 首领预设
+    preset_boss: str = Field(default='', description='preset_boss_help')
+    # 副将预设
+    preset_general: str = Field(default='', description='preset_general_help')
+    # 精英预设
+    preset_elite: str = Field(default='', description='preset_elite_help')
+    # 小蛇预设
+    preset_snake: str = Field(default='', description='preset_snake_help')
+    # 首领策略 等待打完/时间到了退出/伤害足够退出/秒退
+    strategy_boss: str = Field(default='', description='strategy_boss_help')
+    # 副将策略
+    strategy_general: str = Field(default='', description='strategy_general_help')
+    # 精英策略
+    strategy_elite: str = Field(default='', description='strategy_elite_help')
+
+    def parse_order_item(self, str):
+        area, num = str.split('-')
+        result = [str]
+        match area:
+            case 'A':
+                result.append(AbyssShadowsAssets.C_ABYSS_DRAGON)
+            case 'B':
+                result.append(AbyssShadowsAssets.C_ABYSS_PEACOCK)
+            case 'C':
+                result.append(AbyssShadowsAssets.C_ABYSS_FOX)
+            case 'D':
+                result.append(AbyssShadowsAssets.C_ABYSS_LEOPARD)
+            case _:
+                result.append(AbyssShadowsAssets.C_ABYSS_DRAGON)
+        match num:
+            case '1':
+                result.append(AbyssShadowsAssets.C_BOSS_CLICK_AREA)
+            case '2':
+                result.append(AbyssShadowsAssets.C_GENERAL_1_CLICK_AREA)
+            case '3':
+                result.append(AbyssShadowsAssets.C_GENERAL_2_CLICK_AREA)
+            case '4':
+                result.append(AbyssShadowsAssets.C_ELITE_1_CLICK_AREA)
+            case '5':
+                result.append(AbyssShadowsAssets.C_ELITE_2_CLICK_AREA)
+            case '6':
+                result.append(AbyssShadowsAssets.C_ELITE_3_CLICK_AREA)
+            case _:
+                result.append(AbyssShadowsAssets.C_ELITE_1_CLICK_AREA)
+        return tuple(result)
+
+    def parse_order(self, value: str) -> list:
+        if value == '':
+            return []
+        tmp = value.split(';')
+        for item in tmp:
+            result = self.parse_order_item(item)
+            yield result
+
+    def is_need_mark_main(self, code):
+        def expand_mark_main(mark_main_str: str):
+            items = mark_main_str.split(';')
+            expanded = []
+            for item in items:
+                if item == 'A' or item == 'B' or item == 'C' or item == 'D':
+                    expanded.append(item + "-1")
+                    expanded.append(item + "-2")
+                    expanded.append(item + "-3")
+                    expanded.append(item + "-4")
+                    expanded.append(item + "-5")
+                    expanded.append(item + "-6")
+                    continue
+                if item == '1' or item == '2' or item == '3' or item == '4' or item == '5' or item == '6':
+                    expanded.append('A-' + item)
+                    expanded.append('B-' + item)
+                    expanded.append('C-' + item)
+                    expanded.append('D-' + item)
+                    continue
+                expanded.append(item)
+            return expanded
+
+        return code in expand_mark_main(self.mark_main)
+
+
+class SavedParams(ConfigBase):
+    # 已完成
+    done: str = Field(default='', description='done_help')
+    # 失败
+    fail: str = Field(default='', description='fail_help')
+    # 已知的已经打完的
+    unavailable: str = Field(default='', description='unavailable_help')
+    # 当前时间，用于判断存储参数有效性
+    today: str = Field(default='2023-01-01', description='today_help')
+
+    # def save(self):
+    #     self.today = datetime.today().strftime('yyyy-mm-dd')
+    #     self.config.save()
+
 
 class AbyssShadows(ConfigBase):
     scheduler: Scheduler = Field(default_factory=Scheduler)
--- tasks/AbyssShadows/script_task.py
+++ tasks/AbyssShadows/script_task.py
@@ -4,89 +4,24 @@
 # @note     draft version without full test
 # github    https://github.com/roarhill/oas
 
-from datetime import datetime, timedelta
-import random
-import numpy as np
-from enum import Enum
-from cached_property import cached_property
+from datetime import datetime
 from time import sleep
 
-from tasks.Component.GeneralBattle.config_general_battle import GeneralBattleConfig
-from tasks.Component.SwitchSoul.switch_soul import SwitchSoul
-from tasks.Component.GeneralBattle.general_battle import GeneralBattle
-from tasks.Component.config_base import ConfigBase, Time
-from tasks.GameUi.game_ui import GameUi
-from tasks.GameUi.page import page_main, page_kekkai_toppa, page_shikigami_records, page_guild
-from tasks.RealmRaid.assets import RealmRaidAssets
-
-from module.logger import logger
 from module.exception import TaskEnd
-from module.atom.image_grid import ImageGrid
-from module.base.utils import point2str
-from module.base.timer import Timer
-from module.exception import GamePageUnknownError
-from pathlib import Path
-from tasks.AbyssShadows.config import AbyssShadows
+from module.logger import logger
 from tasks.AbyssShadows.assets import AbyssShadowsAssets
-
-class AreaType:
-    """ 暗域类型 """
-    DRAGON = AbyssShadowsAssets.I_ABYSS_DRAGON  # 神龙暗域
-    PEACOCK = AbyssShadowsAssets.I_ABYSS_PEACOCK  # 孔雀暗域
-    FOX = AbyssShadowsAssets.I_ABYSS_FOX  # 白藏主暗域
-    LEOPARD = AbyssShadowsAssets.I_ABYSS_LEOPARD # 黑豹暗域
-
-    @cached_property
-    def name(self) -> str:
-        """
-
-        :return:
-        """
-        return Path(self.file).stem.upper()
-
-    def __str__(self):
-        return self.name
-
-    __repr__ = __str__
-
-class EmemyType(Enum):
-
-    """ 敌人类型 """
-    BOSS = 1  #  首领
-    GENERAL = 2  #  副将
-    ELITE = 3  #  精英
-
-
-class CilckArea:
-    """ 点击区域 """
-    GENERAL_1 = AbyssShadowsAssets.C_GENERAL_1_CLICK_AREA
-    GENERAL_2 = AbyssShadowsAssets.C_GENERAL_2_CLICK_AREA
-    ELITE_1 = AbyssShadowsAssets.C_ELITE_1_CLICK_AREA
-    ELITE_2 = AbyssShadowsAssets.C_ELITE_2_CLICK_AREA
-    ELITE_3 = AbyssShadowsAssets.C_ELITE_3_CLICK_AREA
-    BOSS= AbyssShadowsAssets.C_BOSS_CLICK_AREA
-
-    @cached_property
-    def name(self) -> str:
-        """
-
-        :return:
-        """
-        return Path(self.file).stem.upper()
-
-    def __str__(self):
-        return self.name
-
-    __repr__ = __str__
+from tasks.AbyssShadows.config import AbyssShadows, EnemyType, AreaType, CilckArea
+from tasks.Component.GeneralBattle.general_battle import GeneralBattle
+from tasks.Component.SwitchSoul.switch_soul import SwitchSoul
+from tasks.GameUi.game_ui import GameUi
+from tasks.GameUi.page import page_main, page_shikigami_records, page_guild
 
 
 class ScriptTask(GeneralBattle, GameUi, SwitchSoul, AbyssShadowsAssets):
-    
-
     boss_fight_count = 0  # 首领战斗次数
     general_fight_count = 0  # 副将战斗次数
     elite_fight_count = 0  # 精英战斗次数
-    
+
     def run(self):
         """ 狭间暗域主函数
 
@@ -106,7 +41,8 @@ class ScriptTask(GeneralBattle, GameUi, SwitchSoul, AbyssShadowsAssets):
         if today not in [4, 5, 6]:
             logger.info(f"Today is not abyss shadows day, exit")
             # 设置下次运行时间为本周五
-            self.custom_next_run(task='AbyssShadows', custom_time=cfg.abyss_shadows_time.custom_run_time_friday, time_delta=4-today)
+            self.custom_next_run(task='AbyssShadows', custom_time=cfg.abyss_shadows_time.custom_run_time_friday,
+                                 time_delta=4 - today)
             raise TaskEnd
         success = True
         # 进入狭间
@@ -117,7 +53,7 @@ class ScriptTask(GeneralBattle, GameUi, SwitchSoul, AbyssShadowsAssets):
             self.goto_main()
             self.set_next_run(task='AbyssShadows', finish=False, server=True, success=False)
             raise TaskEnd
-        
+
         # 等待可进攻时间  
         self.device.stuck_record_add('BATTLE_STATUS_S')
         # 集结中图片
@@ -127,13 +63,14 @@ class ScriptTask(GeneralBattle, GameUi, SwitchSoul, AbyssShadowsAssets):
         # 准备攻打精英、副将、首领
         while 1:
             # 点击战报按钮
-            find_list = [EmemyType.BOSS, EmemyType.GENERAL, EmemyType.ELITE]
+            find_list = [EnemyType.BOSS, EnemyType.GENERAL, EnemyType.ELITE]
             for enemy_type in find_list:
                 # 寻找敌人并开始战斗,
                 if not self.find_enemy(enemy_type):
                     logger.warning(f"Failed to find {enemy_type.name} enemy, exit")
                     break
-            logger.info(f"Current fight times: boss {self.boss_fight_count} times, general {self.general_fight_count}  times, elite {self.elite_fight_count} times")
+            logger.info(
+                f"Current fight times: boss {self.boss_fight_count} times, general {self.general_fight_count}  times, elite {self.elite_fight_count} times")
             # 正常应该打完一个区域了，检查攻打次数，如没打够则切换到下一个区域，默认神龙 -> 孔雀 -> 白藏主 -> 黑豹
             if self.boss_fight_count >= 2 and self.general_fight_count >= 4 and self.elite_fight_count >= 6:
                 success = True
@@ -147,13 +84,12 @@ class ScriptTask(GeneralBattle, GameUi, SwitchSoul, AbyssShadowsAssets):
                 elif current_area == AreaType.PEACOCK:
                     self.change_area(AreaType.FOX)
                     continue
-                elif current_area == AreaType.FOX:  
+                elif current_area == AreaType.FOX:
                     self.change_area(AreaType.LEOPARD)
                     continue
                 else:
                     logger.warning("All enemy types have been defeated, but not enough emeny to fight, exit")
                     break
-        
 
         # 保持好习惯，一个任务结束了就返回到庭院，方便下一任务的开始
         self.goto_main()
@@ -163,15 +99,18 @@ class ScriptTask(GeneralBattle, GameUi, SwitchSoul, AbyssShadowsAssets):
             if today == 4:
                 # 周五推迟到周六
                 logger.info(f"The next abyss shadows day is Saturday")
-                self.custom_next_run(task='AbyssShadows', custom_time=cfg.abyss_shadows_time.custom_run_time_saturday, time_delta=1)
+                self.custom_next_run(task='AbyssShadows', custom_time=cfg.abyss_shadows_time.custom_run_time_saturday,
+                                     time_delta=1)
             elif today == 5:
                 # 周六推迟到周日
                 logger.info(f"The next abyss shadows day is Sunday")
-                self.custom_next_run(task='AbyssShadows', custom_time=cfg.abyss_shadows_time.custom_run_time_sunday, time_delta=1)
+                self.custom_next_run(task='AbyssShadows', custom_time=cfg.abyss_shadows_time.custom_run_time_sunday,
+                                     time_delta=1)
             elif today == 6:
                 # 周日推迟到下周五
                 logger.info(f"The next abyss shadows day is Friday")
-                self.custom_next_run(task='AbyssShadows', custom_time=cfg.abyss_shadows_time.custom_run_time_friday, time_delta=5)
+                self.custom_next_run(task='AbyssShadows', custom_time=cfg.abyss_shadows_time.custom_run_time_friday,
+                                     time_delta=5)
         else:
             self.set_next_run(task='AbyssShadows', finish=True, server=True, success=False)
         raise TaskEnd
@@ -207,22 +146,20 @@ class ScriptTask(GeneralBattle, GameUi, SwitchSoul, AbyssShadowsAssets):
             if self.appear(self.I_ABYSS_DRAGON):
                 self.select_boss(area_name)
                 logger.info(f"Switch to {area_name.name}")
-                continue      
-            # 点击战报按钮
-            if self.appear_then_click(self.I_CHANGE_AREA,interval=4):
+                continue
+                # 点击战报按钮
+            if self.appear_then_click(self.I_CHANGE_AREA, interval=4):
                 logger.info(f"Click {self.I_CHANGE_AREA.name}")
                 continue
-                  
+
         return True
-    
+
     def goto_main(self):
         ''' 保持好习惯，一个任务结束了就返回庭院，方便下一任务的开始或者是出错重启
         '''
         self.ui_get_current_page()
         logger.info("Exiting abyss_shadows")
         self.ui_goto(page_main)
-
-       
 
     def goto_abyss_shadows(self) -> bool:
         ''' 进入狭间
@@ -231,23 +168,23 @@ class ScriptTask(GeneralBattle, GameUi, SwitchSoul, AbyssShadowsAssets):
         self.ui_get_current_page()
         logger.info("Entering abyss_shadows")
         self.ui_goto(page_guild)
-        
+
         while 1:
             self.screenshot()
             # 进入神社
-            if self.appear_then_click(self.I_RYOU_SHENSHE,interval=1):
+            if self.appear_then_click(self.I_RYOU_SHENSHE, interval=1):
                 logger.info("Enter Shenshe")
                 continue
             # 查找狭间
             if not self.appear(self.I_ABYSS_SHADOWS, threshold=0.8):
-                self.swipe(self.S_TO_ABBSY_SHADOWS,interval=3)
+                self.swipe(self.S_TO_ABBSY_SHADOWS, interval=3)
                 continue
             # 进入狭间
             if self.appear_then_click(self.I_ABYSS_SHADOWS):
                 logger.info("Enter abyss_shadows")
                 break
         return True
- 
+
     def select_boss(self, area_name: AreaType) -> bool:
         ''' 选择暗域类型
         :return 
@@ -256,13 +193,17 @@ class ScriptTask(GeneralBattle, GameUi, SwitchSoul, AbyssShadowsAssets):
         while 1:
             self.screenshot()
             # 区域图片与入口图片不一致，使用点击进去
-            
+
             if self.appear(self.I_ABYSS_DRAGON):
                 match area_name:
-                    case AreaType.DRAGON: is_click = self.click(self.C_ABYSS_DRAGON,interval=2)
-                    case AreaType.PEACOCK: is_click = self.click(self.C_ABYSS_PEACOCK,interval=2)
-                    case AreaType.FOX: is_click = self.click(self.C_ABYSS_FOX,interval=2)
-                    case AreaType.LEOPARD: is_click = self.click(self.C_ABYSS_LEOPARD,interval=2)
+                    case AreaType.DRAGON:
+                        is_click = self.click(self.C_ABYSS_DRAGON, interval=2)
+                    case AreaType.PEACOCK:
+                        is_click = self.click(self.C_ABYSS_PEACOCK, interval=2)
+                    case AreaType.FOX:
+                        is_click = self.click(self.C_ABYSS_FOX, interval=2)
+                    case AreaType.LEOPARD:
+                        is_click = self.click(self.C_ABYSS_LEOPARD, interval=2)
                 if is_click:
                     click_times += 1
                     logger.info(f"Click {area_name.name} {click_times} times")
@@ -274,7 +215,7 @@ class ScriptTask(GeneralBattle, GameUi, SwitchSoul, AbyssShadowsAssets):
                 break
         return True
 
-    def find_enemy(self, enemy_type: EmemyType) -> bool:
+    def find_enemy(self, enemy_type: EnemyType) -> bool:
         ''' 寻找敌人,并开始寻路进入战斗
         :return 是否找到敌人，若目标已死亡则返回False，否则返回True
         True 找到敌人，并已经战斗完成
@@ -285,15 +226,18 @@ class ScriptTask(GeneralBattle, GameUi, SwitchSoul, AbyssShadowsAssets):
             # 点击战报按钮
             if self.appear(self.I_ABYSS_MAP):
                 break
-            if self.appear_then_click(self.I_ABYSS_NAVIGATION,interval=1):
+            if self.appear_then_click(self.I_ABYSS_NAVIGATION, interval=1):
                 continue
- 
+
         match enemy_type:
-            case EmemyType.BOSS: success = self.run_boss_fight()
-            case EmemyType.GENERAL: success = self.run_general_fight()
-            case EmemyType.ELITE: success = self.run_elite_fight()
-            
-        return success    
+            case EnemyType.BOSS:
+                success = self.run_boss_fight()
+            case EnemyType.GENERAL:
+                success = self.run_general_fight()
+            case EnemyType.ELITE:
+                success = self.run_elite_fight()
+
+        return success
 
     def run_boss_fight(self) -> bool:
         ''' 首领战斗
@@ -304,7 +248,7 @@ class ScriptTask(GeneralBattle, GameUi, SwitchSoul, AbyssShadowsAssets):
             logger.info(f"boss fight count {self.boss_fight_count} times, skip")
             return True
         success = True
-        logger.info(f"Run boss fight") 
+        logger.info(f"Run boss fight")
         if self.click_emeny_area(CilckArea.BOSS):
             logger.info(f"Click {CilckArea.BOSS.name}")
             self.run_general_battle_back()
@@ -319,7 +263,7 @@ class ScriptTask(GeneralBattle, GameUi, SwitchSoul, AbyssShadowsAssets):
         :return 
         '''
         general_list = [CilckArea.GENERAL_1, CilckArea.GENERAL_2]
-        logger.info(f"Run general fight") 
+        logger.info(f"Run general fight")
         for general in general_list:
             # 副将战斗次数达到4个时，退出循环
             if self.general_fight_count >= 4:
@@ -332,13 +276,12 @@ class ScriptTask(GeneralBattle, GameUi, SwitchSoul, AbyssShadowsAssets):
                 logger.info(f'Fight, general_fight_count {self.general_fight_count} times')
         return True
 
-
     def run_elite_fight(self) -> bool:
         ''' 精英战斗
         :return 
         '''
         elite_list = [CilckArea.ELITE_1, CilckArea.ELITE_2, CilckArea.ELITE_3]
-        logger.info(f"Run elite fight")  
+        logger.info(f"Run elite fight")
         for elite in elite_list:
             # 精英战斗次数达到6个时，退出循环
             if self.elite_fight_count >= 6:
@@ -366,7 +309,7 @@ class ScriptTask(GeneralBattle, GameUi, SwitchSoul, AbyssShadowsAssets):
                 continue
             if self.appear(self.I_ABYSS_MAP):
                 logger.info("Find abyss map, exit")
-            
+
             click_times = 0
             # 点击攻打区域
             while 1:
@@ -378,16 +321,16 @@ class ScriptTask(GeneralBattle, GameUi, SwitchSoul, AbyssShadowsAssets):
                 # 出现前往按钮就退出
                 if self.appear(self.I_ABYSS_GOTO_ENEMY):
                     break
-                if self.click(click_area,interval=1.5):
+                if self.click(click_area, interval=1.5):
                     click_times += 1
                     continue
-                if self.appear_then_click(self.I_ENSURE_BUTTON,interval=1):
+                if self.appear_then_click(self.I_ENSURE_BUTTON, interval=1):
                     continue
-        
+
             # 点击前往按钮
             while 1:
                 self.screenshot()
-                if self.appear_then_click(self.I_ABYSS_GOTO_ENEMY,interval=1):
+                if self.appear_then_click(self.I_ABYSS_GOTO_ENEMY, interval=1):
                     logger.info(f"Click {self.I_ABYSS_GOTO_ENEMY.name}")
                     # 点击敌人后，如果是不同区域会确认框，点击确认                
                     if self.appear_then_click(self.I_ENSURE_BUTTON, interval=1):
@@ -397,25 +340,25 @@ class ScriptTask(GeneralBattle, GameUi, SwitchSoul, AbyssShadowsAssets):
                     continue
                 else:
                     break
-            
+
             # 如果遇到点击前往按钮后不动的 bug，则再次尝试进入
             if self.wait_until_appear(self.I_ABYSS_FIRE, wait_time=20):
                 break
             logger.warning("Failed to enter fire")
-        
+
         # 点击战斗按钮
         while 1:
             self.screenshot()
-            if self.appear_then_click(self.I_ABYSS_FIRE,interval=1):
-                  
-                logger.info(f"Click {self.I_ABYSS_FIRE.name}")        
+            if self.appear_then_click(self.I_ABYSS_FIRE, interval=1):
+
+                logger.info(f"Click {self.I_ABYSS_FIRE.name}")
                 # 挑战敌人后，如果是奖励次数上限，会出现确认框   
                 if self.appear_then_click(self.I_ENSURE_BUTTON, interval=1):
                     logger.info(f"Click {self.I_ENSURE_BUTTON.name}")
                 continue
             if self.appear(self.I_PREPARE_HIGHLIGHT):
                 break
-            
+
         return suceess
 
     def run_general_battle_back(self) -> bool:
@@ -455,6 +398,8 @@ class ScriptTask(GeneralBattle, GameUi, SwitchSoul, AbyssShadowsAssets):
 
         return True
 
+    def start_abyss_shadows(self):
+        self.ui_goto(page_guild)
 
 
 if __name__ == "__main__":
