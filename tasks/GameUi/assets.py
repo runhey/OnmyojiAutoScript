595ba527a34cd859c43b1434d6c992847f6f82f1
wanniwa <734933034@qq.com>
2025-02-05 21:55:20 +0800

    fix:修复因为更换检查商店页面的图片，对应位置出现神秘商店老板问题

--- tasks/GameUi/assets.py
+++ tasks/GameUi/assets.py
@@ -153,7 +153,7 @@ class GameUiAssets:
 	# description 
 	I_CHECK_DAILY = RuleImage(roi_front=(27,515,69,62), roi_back=(1,489,155,127), threshold=0.8, method="Template matching", file="./tasks/GameUi/page/page_check_daily.png")
 	# description 
-	I_CHECK_MALL = RuleImage(roi_front=(11,493,93,93), roi_back=(11,493,93,93), threshold=0.8, method="Template matching", file="./tasks/GameUi/page/page_check_mall.png")
+	I_CHECK_MALL = RuleImage(roi_front=(147,527,82,82), roi_back=(147,527,82,82), threshold=0.8, method="Template matching", file="./tasks/GameUi/page/page_check_mall.png")
 	# description 
 	I_CHECK_GUILD = RuleImage(roi_front=(1072,630,49,46), roi_back=(1072,630,49,46), threshold=0.8, method="Template matching", file="./tasks/GameUi/page/page_check_guild.png")
 	# description 
