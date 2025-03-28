6a394201162adfa58f7dbb9471368aafa06a7189
ChengRanORZ <luorq0533@qq.com>
2025-03-28 14:22:32 +0800

    add screenshot

--- tasks/SoulsTidy/script_task.py
+++ tasks/SoulsTidy/script_task.py
@@ -100,6 +100,7 @@ class ScriptTask(GameUi, SoulsTidyAssets):
         logger.hr('Enter bongna')
         # 进入已弃置界面
         while True:
+            self.screenshot()
             if not self.appear(self.I_ST_ABANDONED_SELECTED):
                 self.click(self.I_ST_ABANDONED_SELECTED, interval=2)
                 continue
