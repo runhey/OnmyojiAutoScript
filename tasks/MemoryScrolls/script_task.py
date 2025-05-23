fc5b1f67d4c8211f5ca1988cba03d8758432049d
ghg11 <1550295388@qq.com>
2025-05-23 22:22:24 +0800

    绘卷任务1

--- tasks/MemoryScrolls/script_task.py
+++ tasks/MemoryScrolls/script_task.py
@@ -1,113 +1,69 @@
-from module.base.base import ModuleBase
-from module.exception import TaskError
+# This Python file uses the following encoding: utf-8
+# @author ghg11
+# github https://github.com/ghg11
+from time import sleep
+from enum import Enum
 from module.logger import logger
-from .assets import MemoryScrollsAssets as MS
-from .config import MemoryScrollsConfig, ScrollNumber
+from module.exception import TaskEnd
+from module.base.timer import Timer
+from tasks.GameUi.game_ui import GameUi
+from tasks.GameUi.page import page_summon
+from tasks.MemoryScrolls.assets import MemoryScrollsAssets
+from tasks.MemoryScrolls.config import ScrollNumber
 
 
-class MemoryScrolls(ModuleBase):
-    def __init__(self, config: MemoryScrollsConfig):
-        super().__init__(config)
-        self.config = config
-        self.scroll_map = {
-            ScrollNumber.SCROLL_1: MS.C_MS_SCROLL_1,
-            ScrollNumber.SCROLL_2: MS.C_MS_SCROLL_2,
-            ScrollNumber.SCROLL_3: MS.C_MS_SCROLL_3,
-            ScrollNumber.SCROLL_4: MS.C_MS_SCROLL_4,
-            ScrollNumber.SCROLL_5: MS.C_MS_SCROLL_5,
-            ScrollNumber.SCROLL_6: MS.C_MS_SCROLL_6,
-        }
+class ScriptTask(GameUi, MemoryScrollsAssets):
 
+    # 进入绘卷主界面
     def run(self):
-        """主任务入口"""
-        logger.hr("开始绘卷任务", level=1)
+        self.ui_get_current_page()
+        self.ui_goto(page_summon)
 
-        try:
-            # 1. 进入绘卷界面
-            self._enter()
-
-            # 2. 选择指定分卷
-            self._select_scroll()
-
-            # 3. 处理贡献流程
-            self._process_contribution()
-
-            # 4. 退出界面
-            self._exit()
-
-            logger.info("绘卷任务完成")
-            return True
-        except Exception as e:
-            logger.error(f"任务执行失败: {e}")
-            return False
-
-    def _enter(self):
-        """进入绘卷主界面"""
-        logger.info("尝试进入绘卷界面")
-        for _ in range(3):
-            if self.appear(MS.I_MS_MAIN):
-                break
-            if self.appear(MS.I_MS_ENTER):
-                self.click(MS.C_MS_ENTER)
-                if self.wait_until(MS.I_MS_MAIN):
-                    break
-        else:
-            raise TaskError("无法进入绘卷界面")
-
-    def _select_scroll(self):
-        """选择配置的分卷"""
-        scroll_number = self.config.memory_scrolls_config.scroll_number
-        logger.info(f"选择分卷{scroll_number.value}")
-        self.click(self.scroll_map[scroll_number])
-
-    def _process_contribution(self):
-        """处理碎片贡献流程"""
-        logger.info("开始处理碎片贡献")
-
-        while True:
-            # 检查是否已完成
-            if self.appear(MS.I_MS_COMPLETE):
-                logger.info("当前分卷已完成")
+        # 循环寻找&点击绘卷入口
+        while 1:
+            self.screenshot()
+            if self.appear_then_click(self.I_MS_ENTER, interval=1):
+                continue
+            if self.appear(self.I_MS_MAIN):
                 break
 
-            # 检查是否在正确界面
-            if not self.appear(MS.I_MS_CONTRIBUTE):
-                logger.warning("不在贡献界面，重新开始")
-                return self.run()
-
-            # 处理小碎片
-            if not self.appear(MS.I_MS_ZERO_S):
-                logger.info("贡献小碎片")
-                self.swipe(MS.S_MS_SWIPE_S)
-                self.click(MS.C_MS_CONTRIBUTE)
-                if self.wait_until(MS.I_MS_CONTRIBUTED):
-                    self.click(MS.C_MS_CONTRIBUTED)
+                sleep(3)
+            # (根据config进入指定分卷,不会写所以暂定卷一)
+            if self.appear_then_click(self.I_MS_MAIN, self.C_MS_SCROLL_1, interval=3):
                 continue
+            # 检测到100% 任务完成 退出循环
+            if self.appear(self.I_MS_COMPLETE):
+                logger.info('Scroll already completed')
+                break
 
-            # 处理中碎片
-            if not self.appear(MS.I_MS_ZERO_M):
-                logger.info("贡献中碎片")
-                self.swipe(MS.S_MS_SWIPE_M)
-                self.click(MS.C_MS_CONTRIBUTE)
-                if self.wait_until(MS.I_MS_CONTRIBUTED):
-                    self.click(MS.C_MS_CONTRIBUTED)
-                continue
 
-            # 处理大碎片
-            if not self.appear(MS.I_MS_ZERO_L):
-                logger.info("贡献大碎片")
-                self.swipe(MS.S_MS_SWIPE_L)
-                self.click(MS.C_MS_CONTRIBUTE)
-                if self.wait_until(MS.I_MS_CONTRIBUTED):
-                    self.click(MS.C_MS_CONTRIBUTED)
-                continue
+        # 捐献小碎片直至不足
+        while 1:
+             self.screenshot()
+             if self.appear(self.I_MS_ZERO_S):
+                 break
+             if self.appear(self.I_MS_CONTRIBUTE):
+                 self.swipe(self.S_MS_SWIPE_S,interval=1)
+                 continue
+             if self.appear_then_click(self.I_MS_CONTRIBUTE, interval=1):
+                 continue
+             if self.appear_then_click(self.I_MS_CONTRIBUTED,self.C_MS_CONTRIBUTED,interval=3):
+                 continue
+             continue
 
-            # 所有碎片都已贡献
-            logger.info("当前分卷所有碎片已贡献完毕")
-            break
 
-    def _exit(self):
-        """退出绘卷界面"""
-        logger.info("退出绘卷界面")
-        self.click(MS.C_MS_CLOSE)
-        self.click(MS.C_MS_BACK)
+
+if __name__ == '__main__':
+    from module.config.config import Config
+    from module.device.device import Device
+    c = Config('oas1')
+    d = Device(c)
+    t = ScriptTask(c, d)
+    t.screenshot()
+
+    t.run()
+
+
+
+
+
