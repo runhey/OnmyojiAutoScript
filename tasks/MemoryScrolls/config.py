c95805408c2521732bc536b3ebd5180bc2d22265
yEs1do <yangjiaxiyou@gmail.com>
2025-05-25 02:17:48 +0800

    补全绘卷任务

--- tasks/MemoryScrolls/config.py
+++ tasks/MemoryScrolls/config.py
@@ -10,15 +10,15 @@ from tasks.Component.config_scheduler import Scheduler
 from tasks.Component.config_base import ConfigBase, Time
 
 class ScrollNumber(str, Enum):
-    SCROLL_1 = "卷一"
-    SCROLL_2 = "卷二"
-    SCROLL_3 = "卷三"
-    SCROLL_4 = "卷四"
-    SCROLL_5 = "卷五"
-    SCROLL_6 = "卷六"
+    ONE = "卷一"
+    TWO = "卷二"
+    THREE = "卷三"
+    FOUR = "卷四"
+    FIVE = "卷五"
+    SIX = "卷六"
 
 class MemoryScrollsConfig(ConfigBase):
-    scroll_number: ScrollNumber = Field(default=ScrollNumber.SCROLL_1, description='scroll_number_help')
+    scroll_number: ScrollNumber = Field(default=ScrollNumber.ONE, description='scroll_number_help')
 
 class MemoryScrolls(ConfigBase):
     scheduler: Scheduler = Field(default_factory=Scheduler)
