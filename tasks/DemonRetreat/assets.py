ea4d243ff93b55d85f2bb7e884c6416a8fda4848
MG <vancii1201@163.com>
2025-06-14 22:02:51 +0800

    更新 退治图片坐标

--- tasks/DemonRetreat/assets.py
+++ tasks/DemonRetreat/assets.py
@@ -14,7 +14,7 @@ class DemonRetreatAssets:
 	# 神社 
 	I_SHRINE = RuleImage(roi_front=(870,624,65,61), roi_back=(870,624,65,61), threshold=0.8, method="Template matching", file="./tasks/DemonRetreat/DemonRetreat/DemonRetreat_shrine.png")
 	# 首领退治 
-	I_HUNT = RuleImage(roi_front=(661,164,187,165), roi_back=(661,164,187,165), threshold=0.8, method="Template matching", file="./tasks/DemonRetreat/DemonRetreat/DemonRetreat_hunt.png")
+	I_HUNT = RuleImage(roi_front=(418,162,186,166), roi_back=(418,162,186,166), threshold=0.8, method="Template matching", file="./tasks/DemonRetreat/DemonRetreat/DemonRetreat_hunt.png")
 	# 检查是否成功进入首领退治 
 	I_HUNT_CHECK = RuleImage(roi_front=(572,10,144,46), roi_back=(572,10,144,46), threshold=0.8, method="Template matching", file="./tasks/DemonRetreat/DemonRetreat/DemonRetreat_hunt_check.png")
 	# description 
@@ -26,11 +26,11 @@ class DemonRetreatAssets:
 	# 取消退出 
 	I_QUIT_BACK = RuleImage(roi_front=(488,401,100,44), roi_back=(488,401,100,44), threshold=0.8, method="Template matching", file="./tasks/DemonRetreat/DemonRetreat/DemonRetreat_new.png")
 	# 退治排行榜 
-	I_RANK_LSIT = RuleImage(roi_front=(542,5,200,54), roi_back=(542,5,200,54), threshold=0.8, method="Template matching", file="./tasks/DemonRetreat/DemonRetreat/DemonRetreat_rank_lsit.png")
+	I_RANK_LSIT = RuleImage(roi_front=(538,6,200,54), roi_back=(538,6,200,54), threshold=0.8, method="Template matching", file="./tasks/DemonRetreat/DemonRetreat/DemonRetreat_rank_lsit.png")
 	# 全部领取 
-	I_REWARD_ALL = RuleImage(roi_front=(535,540,177,60), roi_back=(535,540,177,60), threshold=0.8, method="Template matching", file="./tasks/DemonRetreat/DemonRetreat/DemonRetreat_reward_all.png")
+	I_REWARD_ALL = RuleImage(roi_front=(534,540,176,60), roi_back=(534,540,176,60), threshold=0.8, method="Template matching", file="./tasks/DemonRetreat/DemonRetreat/DemonRetreat_reward_all.png")
 	# 祈愿 
-	I_PRAY = RuleImage(roi_front=(1176,385,43,70), roi_back=(1176,385,43,70), threshold=0.8, method="Template matching", file="./tasks/DemonRetreat/DemonRetreat/DemonRetreat_pray.png")
+	I_PRAY = RuleImage(roi_front=(1178,380,42,70), roi_back=(1176,380,42,70), threshold=0.8, method="Template matching", file="./tasks/DemonRetreat/DemonRetreat/DemonRetreat_pray.png")
 
 
 	# Ocr Rule Assets
