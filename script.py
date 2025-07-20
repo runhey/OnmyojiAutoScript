94780fbd6a2c4a400ab885c5c8f8997a609f40bb
Rain1231244 <105040887+Rain1231244@users.noreply.github.com>
2025-07-20 15:33:18 +1000

    Add auto check and delete log file

--- script.py
+++ script.py
@@ -14,6 +14,8 @@ import inflection
 import asyncio
 import json
 
+from datetime import date
+
 from typing import Callable
 from datetime import datetime, timedelta
 from pathlib import Path
@@ -35,6 +37,7 @@ from module.server.i18n import I18n
 
 class Script:
     def __init__(self, config_name: str ='oas') -> None:
+        self.device = None
         logger.hr('Start', level=0)
         self.server = None
         self.state_queue: Queue = None
@@ -310,30 +313,45 @@ class Script:
                 logger.info(f'Wait until {task.next_run} for task `{task.command}`')
                 # self.is_first_task = False
                 method = self.config.script.optimization.when_task_queue_empty
-                if method == 'close_game':
-                    logger.info('Close game during wait')
-                    self.device.app_stop()
-                    self.device.release_during_wait()
-                    if not self.wait_until(task.next_run):
-                        del_cached_property(self, 'config')
-                        continue
-                    self.run('Restart')
-                elif method == 'goto_main':
-                    logger.info('Goto main page during wait')
-                    self.run('GotoMain')
-                    self.device.release_during_wait()
-                    if not self.wait_until(task.next_run):
-                        del_cached_property(self, 'config')
-                        continue
+                close_game_limit_time = self.config.script.optimization.close_game_limit_time
+                close_emulator_limit_time = self.config.script.optimization.close_emulator_limit_time
+
+                if method == 'goto_main':
+                    self._handle_goto_main()
+                elif method == 'close_game':
+                    self._handle_close_game(task, close_game_limit_time)
+                elif method in ['close_emulator_or_goto_main', 'close_emulator_or_close_game']:
+                    self._handle_close_emulator_or(task, close_game_limit_time, close_emulator_limit_time, method)
                 else:
                     logger.warning(f'Invalid Optimization_WhenTaskQueueEmpty: {method}, fallback to stay_there')
-                    self.device.release_during_wait()
-                    if not self.wait_until(task.next_run):
-                        del_cached_property(self, 'config')
-                        continue
+
+                self.device.release_during_wait()
+                if not self.wait_until(task.next_run):
+                    del_cached_property(self, 'config')
+                    continue
             break
 
         return task.command
+
+    def _handle_goto_main(self):
+        logger.info('Goto main page during wait')
+        self.run('GotoMain')
+
+    def _handle_close_game(self, task, close_game_limit_time):
+        if task.next_run > datetime.now() + timedelta(hours=close_game_limit_time.hour, minutes=close_game_limit_time.minute, seconds=close_game_limit_time.second):
+            logger.info('Close game during wait')
+            self.device.app_stop()
+        else:
+            self._handle_goto_main()
+
+    def _handle_close_emulator_or(self, task, close_game_limit_time, close_emulator_limit_time, method):
+        if task.next_run > datetime.now() + timedelta(hours=close_emulator_limit_time.hour, minutes=close_emulator_limit_time.minute, seconds=close_emulator_limit_time.second):
+            logger.info('Close emulator during wait')
+            self.device.emulator_stop()
+        elif method == 'close_emulator_or_goto_main':
+            self._handle_goto_main()
+        else:
+            self._handle_close_game(task, close_game_limit_time)
 
     def run(self, command: str) -> bool:
         """
@@ -404,8 +422,12 @@ class Script:
         """
         logger.set_file_logger(self.config_name)
         logger.info(f'Start scheduler loop: {self.config_name}')
-
+        
+        start_day = date.today()
         while 1:
+            if date.today() > start_day:
+                logger.set_file_logger(self.config_name)
+                start_day = date.today()
             # Check update event from GUI
             # if self.stop_event is not None:
             #     if self.stop_event.is_set():
@@ -424,17 +446,20 @@ class Script:
             #     logger.info('Server or network is recovered. Restart game client')
             #     self.config.task_call('Restart')
 
+            if self.is_first_task:
+                self.device = Device(self.config)
             # Get task
             task = self.get_next_task()
             # 更新 gui的任务
             # Init device and change server
-            _ = self.device
+            # _ = self.device
             # Skip first restart
             if self.is_first_task and task == 'Restart':
                 logger.info('Skip task `Restart` at scheduler start')
                 self.config.task_delay(task='Restart', success=True, server=True)
                 del_cached_property(self, 'config')
                 continue
+            self.device = Device(self.config)
 
             # Run
             logger.info(f'Scheduler: Start task `{task}`')
