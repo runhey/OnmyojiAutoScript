63c2f0ee0285c4c235e9172c52e03fb81f4347ec
Rain1231244 <105040887+Rain1231244@users.noreply.github.com>
2025-07-21 20:55:14 +1000

    Add files via upload

--- module/logger.py
+++ module/logger.py
@@ -120,8 +120,7 @@ class RichFileHandler(RichHandler):
 pyw_name = os.path.splitext(os.path.basename(sys.argv[0]))[0]
 
 
-def set_file_logger(name=pyw_name):
-    cleanup_logs("./log", keep_days=7)   # 先清理旧日志
+def set_file_logger(name=pyw_name, *, do_cleanup=False):
     if '_' in name:
         name = name.split('_', 1)[0]
     log_file = f'./log/{datetime.date.today()}_{name}.txt'
@@ -155,6 +154,12 @@ def set_file_logger(name=pyw_name):
         h, (logging.FileHandler, RichFileHandler))]
     logger.addHandler(hdlr)
     logger.log_file = log_file
+
+    # ---------- 可选：清理旧文件 ----------
+    if do_cleanup:
+        cleanup_logs()
+    if do_cleanup:
+        logger.info("Log cleanup finished")   
 
 
 # ======================================================================================================================
