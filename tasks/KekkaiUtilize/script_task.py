5e9cb067c1702f9306a02ac89ae0606a39f09ffb
yEs1do <yangjiaxiyou@gmail.com>
2025-08-16 18:25:19 +0800

    fix

--- tasks/KekkaiUtilize/script_task.py
+++ tasks/KekkaiUtilize/script_task.py
@@ -78,7 +78,7 @@ class ScriptTask(GameUi, ReplaceShikigami, KekkaiUtilizeAssets):
         if ap_enable or assets_enable:
             # 尝试移动寻找体力或资金
             try_find_ap = 0
-            while try_find_ap < 2:
+            while try_find_ap < 3:
                 self.screenshot()
                 try_find_ap += 1
                 if self.appear(self.I_GUILD_AP) or self.appear(self.I_GUILD_ASSETS):
@@ -86,7 +86,7 @@ class ScriptTask(GameUi, ReplaceShikigami, KekkaiUtilizeAssets):
                     break
                 else:
                     logger.info('Try find ap or assets')
-                    self.swipe(self.S_GUILD_FIND_AP, interval=3)
+                    self.swipe(self.S_GUILD_FIND_AP)
             # 如果未找到则返回False
             if not self.appear(self.I_GUILD_AP) and not self.appear(self.I_GUILD_ASSETS):
                 logger.info('No ap or assets to collect')
