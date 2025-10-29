86fb2078c488ea68f06d1ffd9426e34920743e90
Azure <bluetian1st@gmail.com>
2025-10-29 21:04:04 +0800

    feat(GameUI): auto register page

--- tasks/GameUi/game_ui.py
+++ tasks/GameUi/game_ui.py
@@ -2,11 +2,17 @@
 # @author runhey
 # github https://github.com/runhey
 import time
+
+import importlib
+import pkgutil
+from pathlib import Path
+
 from datetime import datetime
 from time import sleep
 
 import random
 from collections import deque
+from module.atom.click import RuleClick
 from module.atom.gif import RuleGif
 from module.atom.image import RuleImage
 from module.atom.list import RuleList
@@ -15,8 +21,9 @@ from module.base.decorator import run_once
 from module.base.timer import Timer
 from module.exception import (GameNotRunningError, GamePageUnknownError)
 from module.logger import logger
+from tasks.Component.GeneralBattle.assets import GeneralBattleAssets
 from tasks.GameUi.assets import GameUiAssets
-from tasks.GameUi.page import *
+from tasks.GameUi.page import Page, PageRegistry, page_main
 from tasks.Restart.assets import RestartAssets
 from tasks.SixRealms.assets import SixRealmsAssets
 from tasks.base_task import BaseTask
@@ -25,26 +32,6 @@ from tasks.ActivityShikigami.assets import ActivityShikigamiAssets
 
 class GameUi(BaseTask, GameUiAssets):
     ui_current: Page = None
-    ui_pages = [
-        # 登录
-        page_login,
-        # 主页
-        page_main, page_summon, page_exploration, page_town,
-        # 探索的
-        page_awake_zones, page_soul_zones, page_realm_raid, page_goryou_realm, page_delegation,
-        page_secret_zones, page_area_boss, page_heian_kitan, page_six_gates, page_bondling_fairyland,
-        page_kekkai_toppa,
-        # 町中的
-        page_duel, page_demon_encounter, page_hunt, page_hunt_kirin, page_draft_duel, page_hyakkisen,
-        # 庭院里面的
-        page_shikigami_records, page_onmyodo, page_friends, page_daily, page_mall, page_guild, page_team,
-        page_collection, page_act_list,
-        # 爬塔活动
-        page_act_list_climb_act, page_climb_act, page_climb_act_2, page_climb_act_pass, page_climb_act_ap,
-        page_climb_act_boss, page_climb_act_buff, page_climb_act_ap100,
-        # 战斗
-        page_battle, page_reward, page_failed
-    ]
     ui_close = [GameUiAssets.I_BACK_MALL, GeneralBattleAssets.I_CONFIRM,
                 BaseTask.I_UI_BACK_RED, BaseTask.I_UI_BACK_YELLOW,
                 GameUiAssets.I_BACK_FRIENDS, GameUiAssets.I_BACK_DAILY,
@@ -52,6 +39,31 @@ class GameUi(BaseTask, GameUiAssets):
                 GameUiAssets.I_SIX_GATES_GOTO_EXPLORATION, SixRealmsAssets.I_EXIT_SIXREALMS,
                 ActivityShikigamiAssets.I_SKIP_BUTTON, ActivityShikigamiAssets.I_RED_EXIT, BaseTask.I_UI_BACK_BLUE,
                 ActivityShikigamiAssets.I_RED_EXIT_2]
+
+    def __init__(self, config, device):
+        super().__init__(config, device)
+        # 初始化时动态导入所有 page 模块
+        self._import_all_pages()
+
+    @staticmethod
+    def _import_all_pages():
+        """动态加载 tasks/**/page.py"""
+        base_dir = Path(__file__).resolve().parent.parent  # tasks 目录
+        for task_dir in base_dir.iterdir():
+            if not task_dir.is_dir():
+                continue
+            page_file = task_dir / "page.py"
+            if not page_file.exists():
+                continue
+            module_name = f"tasks.{task_dir.name}.page"
+            spec = importlib.util.spec_from_file_location(module_name, page_file)
+            if spec and spec.loader:
+                module = importlib.util.module_from_spec(spec)
+                spec.loader.exec_module(module)
+
+    @property
+    def ui_pages(self) -> list[Page]:
+        return PageRegistry.all()
 
     def home_explore(self) -> bool:
         """
