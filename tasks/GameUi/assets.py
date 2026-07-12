12eaff8bc7c2a06f38c913bdf6da025bf74983ad
neptine <2234044577@qq.com>
2026-07-12 20:01:13 +0800

    fix(GameUi): Refine six gates page detection ROI to avoid misidentifying BondlingFairyland and support all 6 realms  #1673

--- tasks/GameUi/assets.py
+++ tasks/GameUi/assets.py
@@ -89,7 +89,7 @@ class GameUiAssets:
 	# description 
 	I_CHECK_HEIAN_KITAN = RuleImage(roi_front=(27,48,47,39), roi_back=(27,48,47,39), threshold=0.8, method="Template matching", file="./tasks/GameUi/page/page_check_heian_kitan.png")
 	# 六道之门检查 
-	I_CHECK_SIX_GATES = RuleImage(roi_front=(411,297,21,21), roi_back=(394,252,58,104), threshold=0.85, method="Template matching", file="./tasks/GameUi/page/page_check_six_gates.png")
+	I_CHECK_SIX_GATES = RuleImage(roi_front=(411,297,21,21), roi_back=(395,262,54,85), threshold=0.85, method="Template matching", file="./tasks/GameUi/page/page_check_six_gates.png")
 	# description 
 	I_CHECK_BONDLING_FAIRYLAND = RuleImage(roi_front=(614,660,56,49), roi_back=(614,660,56,49), threshold=0.8, method="Template matching", file="./tasks/GameUi/page/page_check_bondling_fairyland.png")
 	# description 
